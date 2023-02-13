ca4198c419609ae45390cdc7e69a42dc04973fce
Jenny <jennyvo3535@gmail.com>
2023-02-12 16:08:31 -0800

    link to details page

--- color.js
+++ color.js
@@ -7,10 +7,11 @@ let Color = function(colorID,R,G,B) {
     
 };
 
-Color.prototype.displayColor = function(){
+Color.prototype.displayColor = function(colorClassName){
     let aColor = document.createElement("div");
-    aColor.className = this.colorName;
+    aColor.className = colorClassName;
     aColor.id = this.colorName;
+    aColor.setAttribute("data-parm", aColor.id);
     aColor.textContent = `ColorID ${this.colorID} :  RGB ${this.valueR } : ${this.valueG } : ${this.valueB}`;
     aColor.style.background = "rgb(" + this.valueR  + "," + this.valueG + "," + this.valueB + ")";
     return aColor;
--- main.js
+++ main.js
@@ -13,18 +13,37 @@ function showRandomColorList (){
     let randomColor = createObject();
     randomColorList.push(randomColor);
     let indexOfRandomColor = randomColorList.indexOf(randomColor);
-    appendAColor(randomColorList[indexOfRandomColor], "random-color-list");
+    appendAColor(randomColorList[indexOfRandomColor], "random-color-list", "random-color-classname");
+    activateAColor("random-color-classname");
+        // need one for our details page to fill in the info based on the passed in ID
+    $(document).on("pagebeforeshow", "#details", function (event) {   
+        let colorElementID = localStorage.getItem('colorName');  // get the unique key back from the storage dictionairy
+        document.getElementById("test-para").textContent = "Color's detail: " + colorElementID;
+    });
 };
 
 function showAllColors (){
-    
-    displayAllColors(randomColorList, "list-library-color");
+    displayAllColors(randomColorList, "list-library-color", "#library-color-classname");
+};
+
+function activateAColor(aColorToActivate){
+    let arrayRandomColor = document.querySelectorAll(`.${aColorToActivate}`);
+    arrayRandomColor.forEach((element, i)=> {     
+        element.addEventListener('click', function () {     
+            
+            var colorIDName = this.getAttribute("data-parm");  // data-parm has this movie object's ID value
+            // now save THIS ID value in the localStorage "dictionairy"
+            localStorage.setItem('colorName', colorIDName);
+            document.location.href = "index.html#details";  // this will jump us to our #details page
+        });
+    });
 };
 
 // Utility functions 
-function displayAllColors(colorList, allColorContainerName){
+function displayAllColors(colorList, allColorContainerName, colorClassName){
+    document.getElementById(allColorContainerName).textContent = "";
     colorList.forEach((item, index)=>{
-        appendAColor(item,allColorContainerName);
+        appendAColor(item, allColorContainerName, colorClassName);
     });
 };
 
@@ -38,9 +57,9 @@ function createObject (){
 };
 
 // Append the created object color to the page
-function appendAColor(aColor, listContainerName){
+function appendAColor(aColor, listContainerName, colorClassName){
     
     let pageRandomColorPlaceHolder = document.getElementById(listContainerName);  
-    pageRandomColorPlaceHolder.append(aColor.displayColor());
+    pageRandomColorPlaceHolder.append(aColor.displayColor(colorClassName));
 
 };
